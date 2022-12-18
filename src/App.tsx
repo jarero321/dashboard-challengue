22d85af16b80c3995aad5e2c44d1709e6109f3f2
luis Carlos Jarero Martinez <jareroluis@gmail.com>
2022-12-18 00:25:22 -0600

    refactor/moving-router-logic-to-component-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+import { PrivateRoute } from "./components/PrivateRoute";
+
 function App() {
-  return <div className="App"></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/dashboard",
+      element: (
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { AxiosInterceptor } from "./interceptors/axios.interceptor";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Login } from "./pages/Login";
-import { Dashboard } from "./pages/Dashboard";
-import { PrivateRoute } from "./components/PrivateRoute";
 import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <PrivateRoute>
-        <Dashboard />
-      </PrivateRoute>
-    ),
-  },
-]);
+import App from "./App";
 
 AxiosInterceptor();
 
@@ -31,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <PersistGate loading={null} persistor={persistor}>
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <App />
       </Provider>
     </PersistGate>
   </React.StrictMode>
